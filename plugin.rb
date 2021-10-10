0b8116e6dd88e484bc3d6170f12c71e16a1921c1
Vikas Dangi <vikasdangi379@gmail.com>
2021-10-10 12:52:18 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: custom-locales
+# about: An example plugin for adding new locales.
+# version: 1.0
+
+register_locale("hi_IN", name: "Hindi (India)", nativeName: "हिंदी (भारत)", fallbackLocale: "es")
